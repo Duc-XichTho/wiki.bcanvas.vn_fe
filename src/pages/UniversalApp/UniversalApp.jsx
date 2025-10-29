f190af06936fbc5043400289c54ddf17a6f27220
Minh Nhat <nhat511111@gmail.com>
2025-10-29 16:28:34 +0700

    uni

--- src/pages/UniversalApp/UniversalApp.jsx
+++ src/pages/UniversalApp/UniversalApp.jsx
@@ -2234,7 +2234,7 @@ const DataRubikProcessGuide = () => {
                 </div>
               ) : (
                 <div>
-                 <div className={styles.processItemActionsContainer} style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '8px', gap: '8px', alignItems: 'center', position: 'sticky', top: '0px', zIndex: 1000, background: '#FDFDFD' }}>
+                 <div className={styles.processItemActionsContainer} style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '8px', gap: '8px', alignItems: 'center', position: 'sticky', top: '-14px', zIndex: 1000, background: '#FDFDFD', height: '44px' }}>
                     {/* Content Search Bar */}
                     <AutoComplete
                       value={contentSearchValue}
